403cb685c9d138d564ab983bd2d072804bb146fe
s3anaXD <oskarraszkowski4@gmail.com>
2026-04-08 21:52:17 +0200

    Wygląd Na tel gotowy

--- obj/Debug/net10.0/FinanceTracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/FinanceTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7fe172500cd411b7813dac8d53fd58ddab713bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9323d82293b4ecb30e75825abfdbd8eed74573f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
