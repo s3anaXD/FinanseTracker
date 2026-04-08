a8cb11cbd070eff6d45ec6c8c2dc86b2687c8ddc
s3anaXD <oskarraszkowski4@gmail.com>
2026-04-08 22:06:49 +0200

    FinanceTracker_v1.1

--- Program.cs
+++ Program.cs
@@ -6,11 +6,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    databaseUrl = databaseUrl.Replace("postgres://", "postgresql://");
+    var dbUri = new Uri(databaseUrl);
+    var userInfo = dbUri.UserInfo.Split(':');
+    connectionString = $"Host={dbUri.Host};Port={dbUri.Port};Database={dbUri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};Ssl Mode=Require;Trust Server Certificate=true;";
+}
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
+
+// Automatically run EF Core Migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 
