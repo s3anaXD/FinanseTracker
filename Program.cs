80f80e2078c5afa0651900d7892876947b4d7838
s3anaXD <oskarraszkowski4@gmail.com>
2026-04-08 22:19:23 +0200

    Fix Npgsql DateTime Unspecified / Local exception mapping by enabling legacy timestamp behavior

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using FinanceTracker.Models;
 using Microsoft.EntityFrameworkCore;
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
